557c8ecc3449500b869dc5d4a671ee68b1d93577
trydev <trydev1995@gmail.com>
2025-05-03 16:12:02 +0700

    add theme

--- lib/app.dart
+++ lib/app.dart
@@ -9,6 +9,24 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Advanced Mobile App',
+      theme: ThemeData(
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.red,
+        ),
+        appBarTheme: AppBarTheme(foregroundColor: Colors.white),
+        brightness: Brightness.light,
+        primaryColor: Colors.green,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          secondary: Colors.deepOrangeAccent,
+        ),
+        fontFamily: 'Angkor',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 36, fontStyle: FontStyle.italic),
+          bodyMedium: TextStyle(fontSize: 14, fontFamily: 'Hin'),
+        ),
+      ),
       home: Home(),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,24 +6,38 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(),
-      body: _buildBody(),
+      appBar: _buildAppBar(context),
+      body: _buildBody(context),
+      floatingActionButton: _buildFAB(),
     );
   }
-}
 
-_buildAppBar() {
-  return AppBar(
-    title: Text('Advanced Mobile App'),
-    centerTitle: true,
-  );
-}
+  _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: Text('Advanced Mobile App'),
+      centerTitle: true,
+    );
+  }
 
-_buildBody() {
-  return Center(
-    child: Text(
-      'សូមអនុវត្តន៍ទាំងអស់គ្នា',
-      style: TextStyle(fontFamily: 'Angkor', fontSize: 28),
-    ),
-  );
+  _buildBody(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.secondary,
+      child: Center(
+        child: Text(
+          'សូមអនុវត្តន៍ទាំងអស់គ្នា',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+
+  _buildFAB() {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: Icon(
+        Icons.arrow_circle_up,
+      ),
+    );
+  }
 }
